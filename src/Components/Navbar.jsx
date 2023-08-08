593e664654ae5fe26d12633ed188bd5e646a0a2f
Jose Moreno <josexmoreno1998@gmail.com>
2023-08-07 20:17:10 -0400

    Adding a tags to the navbar links and specifying the cursor when hover

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,11 +6,21 @@ function Navbar() {
     <div className="flex justify-between items-center w-full">
       <img src={logo} alt="Company logo" />
       <ul className="flex p-0">
-        <li>Home</li>
-        <li className="ml-6">New</li>
-        <li className="ml-6">Popular</li>
-        <li className="ml-6">Trending</li>
-        <li className="ml-6">Categories</li>
+        <li className="cursor-pointer">
+          <a>Home</a>
+        </li>
+        <li className="ml-6 cursor-pointer">
+          <a>New</a>
+        </li>
+        <li className="ml-6 cursor-pointer">
+          <a>Popular</a>
+        </li>
+        <li className="ml-6 cursor-pointer">
+          <a>Trending</a>
+        </li>
+        <li className="ml-6 cursor-pointer">
+          <a>Categories</a>
+        </li>
       </ul>
     </div>
   );
